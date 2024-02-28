8936e178106478859c77a0b595ce5126cea6c816
ulan <ulan.aliekov@gmail.com>
2024-02-28 07:01:15 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="main">
+    <section class="main-left">sada</section>
+    <section class="main-right">fdsf</section>
+  </main>
+</template>
+
+<style>
+  .main {
+    display: flex;
+  }
+
+  .main-left {
+    flex: 1 1 240px;
+    background-color: #F5F6F8;
+    border-right: 1px solid #E6E9EF;
+  }
+
+  .main-right {
+    padding: 30px;
+    background-color: #F5F6F8;
+    border: 1px solid #E6E9EF;
+  }
+</style>
